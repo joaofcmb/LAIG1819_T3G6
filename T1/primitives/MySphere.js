393692e65e48b30d22e7290cad691cbb29c8f469
joaofcmb <joaofernandomeireles@gmail.com>
2018-10-09 20:10:42 +0100

    Fixed Sphere texture coordinates (normals slightly improved)

--- T1/primitives/MySphere.js
+++ T1/primitives/MySphere.js
@@ -24,29 +24,28 @@ class MySphere extends CGFobject
 		this.normals = [];
 
 		var hAngle = 2* Math.PI / this.slices;
-		var vAngle = Math.PI / (2* this.stacks);
+		var vAngle = Math.PI / this.stacks;
 
-		for (var i = 0; i < this.stacks * 2 + 2; i++) {
-			for (var j = 0; j < this.slices * 2; j++) {
+		var halfStacks = this.stacks / 2;
+
+		for (var i = - halfStacks; i < halfStacks + 1; i++) {
+			for (var j = 0; j < this.slices + 1; j++) {
 				this.vertices.push(	Math.cos(hAngle * j) * Math.cos(vAngle * i) * this.radius,
 									Math.sin(hAngle * j) * Math.cos(vAngle * i) * this.radius,
 									Math.sin(vAngle * i) * this.radius);
-				this.texCoords.push((Math.cos(hAngle * j) * Math.cos(vAngle * i)+1) / 2, (1-(Math.sin(hAngle * j) * Math.cos(vAngle * i)+1) / 2));
-				this.normals.push(Math.cos(hAngle * j), Math.sin(hAngle * j), Math.sin(vAngle * i));
+				this.texCoords.push(j / this.slices, 1 - ((i + halfStacks) / this.stacks));
+				this.normals.push(Math.cos(hAngle * j) * Math.cos(vAngle * i), Math.sin(hAngle * j) * Math.sin(vAngle * j), Math.sin(vAngle * i));
 			}
 		}
 
 		// DRAW INDICES ------------
-
 		this.indices = [];
 
-		for (var j = 0; j < this.stacks * 2; j++) {
-			for (var i = 0; i < this.slices * 2 - 2; i++) {
-				this.indices.push(i + j * this.slices, i + 1 + j * this.slices, i + (j + 1) * this.slices);
-				this.indices.push(i + 1 + j * this.slices, i + 1 + (j + 1) * this.slices, i + (j + 1) * this.slices);
+		for (var j = 0; j < this.stacks; j++) {
+			for (var i = 0; i < this.slices; i++) {
+				this.indices.push(i + j * (this.slices + 1), i + 1 + j * (this.slices + 1), i + (j + 1) * (this.slices + 1));
+				this.indices.push(i + 1 + j * (this.slices + 1), i + 1 + (j + 1) * (this.slices + 1), i + (j + 1) * (this.slices + 1));
 			}
-			this.indices.push(this.slices - 1 + j * this.slices, j * this.slices, (2 + j) * this.slices - 1);
-			this.indices.push(j * this.slices, (j + 1) * this.slices, (2 + j) * this.slices - 1);
 		}
 
 		// Logs
@@ -76,12 +75,5 @@ class MySphere extends CGFobject
         */
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
-	};
-
-	display() 
-	{
-		super.display();
-		this.scene.rotate(Math.PI, 0, 1, 0);
-		super.display();
 	};
 };

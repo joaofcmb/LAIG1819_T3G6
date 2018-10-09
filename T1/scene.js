b785deb4e48ae3be2e819c6e24c59f4e2734355e
Rui Guedes <ruiguedes12@hotmail.com>
2018-10-09 23:50:50 +0100

    Desenvolvimento do XML final

--- T1/scene.js
+++ T1/scene.js
@@ -43,7 +43,7 @@ class Scene extends CGFscene {
      * Initializes the scene cameras.
      */
     initCameras() {
-        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(75, 75, 75), vec3.fromValues(0, 0, 0));
     }
 
     /**

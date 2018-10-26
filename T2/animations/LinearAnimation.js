ecf1c080d8efd96190225e20a8e73ab2c4597ee7
joaofcmb <joaofernandomeireles@gmail.com>
2018-10-26 13:26:14 +0100

    Started working on Linear Animation

--- T2/animations/LinearAnimation.js
+++ T2/animations/LinearAnimation.js
@@ -7,13 +7,30 @@ class LinearAnimation extends Animation {
         super(scene);
 
         this.span = span;
-        this.controlPoints = controlPoints;
-        this.
+        this.segmentDirections = [];
+        this.segmentDistances = [];
+        this.totalDistance = 0;
 
-        for ()
+        for (var i = 0; i < this.controlPoints.length - 1; i++) {
+            var direction = vec3.create();
+            vec3.subtract(direction, controlPoints[i], controlPoints[i + 1]);
+
+            var distance = distance(controlPoints[i], controlPoints[i + 1]);
+
+            this.segmentDirections.push(direction);
+            this.segmentDistances.push(distance);
+            this.totalDistance += distance;
+        }
     }
-
+    
     update(deltaTime) {
+        var segmentI = 0;
+        var animDirection = vec3.create();
+        var deltaDistance;
 
+        do {
+            deltaDistance = 
+
+        } while(deltaDistance > this.segmentDistances[segmentI])
     }
 }

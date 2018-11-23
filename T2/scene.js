022d55afbf9c3f3202efebd21c317087a24ba8ec
joaofcmb <joaofernandomeireles@gmail.com>
2018-11-23 20:06:35 +0000

    Almost finished with the animations

--- T2/scene.js
+++ T2/scene.js
@@ -63,15 +63,11 @@ class Scene extends CGFscene {
 
         var cam = null;
 
-        console.log(this.data.perspectiveCams);
-
         if ((cam = this.data.perspectiveCams[this.interface.Views]) != null)
             this.camera = new CGFcamera(Math.PI * cam.angle / 180, cam.near, cam.far, vec3.fromValues(cam.fromX, cam.fromY, cam.fromZ), vec3.fromValues(cam.toX, cam.toY, cam.toZ));
         else if ((cam = this.data.orthoCams[this.interface.Views]) != null)
             this.camera = new CGFcameraOrtho(cam.left, cam.right, cam.bottom, cam.top, cam.near, cam.far, vec3.fromValues(cam.fromX, cam.fromY, cam.fromZ),
                 vec3.fromValues(cam.toX, cam.toY, cam.toZ), vec3.fromValues(0, 1, 0));
-
-        console.log(this.camera);
 
         this.interface.setActiveCamera(this.camera);
     }
